9b4d87bcce21432f45d1ddb275f64e56358d9996
Andrew <shixish@gmail.com>
2013-07-07 13:42:48 -1000

    Counts are now stored by the userpoints module. The userpoints module should handle the expiration functionality for us using cron.

---
+++ README.txt
@@ -0,0 +1,5 @@
+This module has been created to keep track of how many downloads each user has made, and limit their usage per some number of days/months.
+
+The module makes use of Rules and the Userpoints module.
+
+Note: This module's settings page is only accessible if the user has the "administer userpoints" permission.
--- file_access_limited.tpl.php
+++ file_access_limited.tpl.php
@@ -1,9 +1,9 @@
 <?php
  global $user;
- $file_count = is_array($user->data)&&isset($user->data['file_count'])?$user->data['file_count']:0;
- $formatted_file_count = format_plural($file_count, '1 file', '@count files');
+ $tid = variable_get('yodify_links_category_tid', 0);
+ $points = format_plural(userpoints_get_current_points($user->uid, $tid), '1 file', '@count files');
 ?>
 
 <h2>Unable to access the requested file at this time</h2>
 The system limits the number of files that can be downloaded per month.<br>
-You have downloaded <strong><?php echo $formatted_file_count ?></strong> this month. Please try again at a later date.<br>
+You have downloaded <strong><?php echo $points ?></strong> this month. Please try again at a later date.<br>
